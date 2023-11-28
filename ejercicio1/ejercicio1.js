4f68cde9ce274398d65efd72d78b08ae05e42f92
Arkant04 <delacorte19@gmail.com>
2023-11-28 14:55:24 +0100

    MOD: Se soluciona el error

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -4,10 +4,15 @@
 function createP(){
     const datos = document.querySelector("#userText").value 
     var elemento = document.createElement("p")
+    console.log(datos)
     console.log(elemento)
 
-    elemento.innerHtml = datos
+    elemento.innerHTML = datos
+    console.log(elemento)
+
     const contenedor = document.querySelector("#contenedor")
+    console.log(contenedor)
+
     contenedor.appendChild(elemento)
     console.log(elemento)
 }
