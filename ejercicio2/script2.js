b9726b6513ec29bddc894926365e31c90202a2fb
Arkant04 <delacorte19@gmail.com>
2023-11-30 17:01:19 +0100

    MOD: dados 2.0

--- ejercicio2/script2.js
+++ ejercicio2/script2.js
@@ -1,25 +1,15 @@
-let cantidadDados = [1,	2,	3,	4,	5,	6,	7,	8,	9,	10,
-    11,	12,	13,	14,	15,	16,	17,	18,	19,	20]
-let carasDados = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 
-    14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 
-    28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 
-    42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 
-    56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 
-    70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 
-    84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 
-    98, 99, 100]
-    var numerosRandom = Math.floor(Math.random() * 100)
-
 function tirarDado(){
     const data = document.querySelector("#inputDado").value
     console.log(data)
     const tirada = data.split("d")
     console.log(tirada)
     const misDados = []
-    for(let i = 0; end ; i++){
-       
+    console.log(tirada[0])
+    for(let i = 0; i< tirada[0] ; i++){
+       const resultado = Math.floor(Math.random() * tirada[1]) +1
+       misDados.push(resultado)
     }
-
+    return misDados
 }
 
 
